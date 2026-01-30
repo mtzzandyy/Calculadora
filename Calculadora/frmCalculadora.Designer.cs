f779a6a210b9eddaa697662ee80c5a08f358a881
andre <24130813@correo.itlalaguna.edu.mx>
2026-01-30 15:46:13 -0600

    hice algunos cambios y agregue un nuevo diseño

---
+++ Calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtVariableA = new TextBox();
+            txtVariableB = new TextBox();
+            btnCalcular = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(255, 128, 128);
+            label1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold | FontStyle.Italic);
+            label1.Location = new Point(93, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Variable A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 128, 128);
+            label2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold | FontStyle.Italic);
+            label2.Location = new Point(93, 226);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Variable B";
+            // 
+            // txtVariableA
+            // 
+            txtVariableA.Location = new Point(109, 101);
+            txtVariableA.Name = "txtVariableA";
+            txtVariableA.Size = new Size(150, 31);
+            txtVariableA.TabIndex = 2;
+            // 
+            // txtVariableB
+            // 
+            txtVariableB.Location = new Point(109, 280);
+            txtVariableB.Name = "txtVariableB";
+            txtVariableB.Size = new Size(150, 31);
+            txtVariableB.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.BackColor = Color.FromArgb(255, 192, 192);
+            btnCalcular.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnCalcular.ForeColor = SystemColors.ActiveCaptionText;
+            btnCalcular.Location = new Point(319, 161);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(112, 54);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = false;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // frmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtVariableB);
+            Controls.Add(txtVariableA);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmCalculadora";
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtVariableA;
+        private TextBox txtVariableB;
+        private Button btnCalcular;
+    }
+}
---
+++ Calculadora/frmCalculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                int a = 0, b = 0, resultado = 0;
+                a = Convert.ToInt32(txtVariableA.Text);
+                b = int.Parse(txtVariableB.Text);
+           
+                resultado = a + b;
+                MessageBox.Show("El resultado es: " + resultado.ToString(), "Sistema",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error a la conversión de datos", "Sistema",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+
+        }
+    }
+}
